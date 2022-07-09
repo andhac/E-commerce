0cbaa7cf0afbefd919d0d173f835723fd971e38e
andhac <andhac84@yahoo.com>
2022-07-09 12:00:07 +0530

    first commit

---
+++ build/precache-manifest.ea0095abe1e508c019ce93ef7f534430.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "439aced5fca95a578f4de1c0f7c42382",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c8741647cd4e5c878623",
+    "url": "/static/js/2.b1e5598b.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.b1e5598b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "fd21ffec06fe9383ebdc",
+    "url": "/static/js/main.ed4c43d0.chunk.js"
+  },
+  {
+    "revision": "26fe881cc3d132d0250d",
+    "url": "/static/js/runtime-main.c9a529bb.js"
+  }
+]);
